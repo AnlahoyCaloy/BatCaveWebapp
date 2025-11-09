d367c5d2151c4ae2d85ffb1f815822f1be9522a1
Sebasch Amauri Santillan <sebaschamauri.09@gmail.com>
2025-11-10 00:25:56 +0800

    add font

--- Frontend/src/app/page.tsx
+++ Frontend/src/app/page.tsx
@@ -8,7 +8,10 @@ export default function Home() {
   return (
     <div className="home flex justify-center items-center w-full flex-col"> 
       <HomeSearch />
-      <h1 className="font-(var(--font-Cinzel))">Explore Drinks</h1>
+      <h1 style={{
+        fontFamily : 'var(--font-Cinzel)',
+        fontSize : '35px'
+      }}>Explore Drinks</h1>
     </div>
   )
 }
