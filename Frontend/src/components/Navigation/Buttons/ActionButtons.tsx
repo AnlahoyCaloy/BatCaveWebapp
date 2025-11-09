e778a90c33b7a98b656cdd6769f8119a55d34488
Sebasch Amauri Santillan <sebaschamauri.09@gmail.com>
2025-11-09 21:33:24 +0800

    fixed the navbar and added fonts and videos

--- Frontend/src/components/Navigation/Buttons/ActionButtons.tsx
+++ Frontend/src/components/Navigation/Buttons/ActionButtons.tsx
@@ -2,7 +2,7 @@
 import React, { forwardRef, useRef, useImperativeHandle, useState } from "react"
 import { motion, AnimatePresence, scale } from "framer-motion"
 import '../../../app/globals.css'
-
+import Link from "next/link"
 export interface ActionButtonRef {
   container: HTMLDivElement | null
 }
@@ -29,41 +29,42 @@ const ActionButtons = forwardRef<ActionButtonRef, ActionButtonProps>(
 
 
     const buttons = navs.map((nav, i) => (
-      <motion.div
-        key={i}
-        className={`action-button ${className}`}
-        style={{
-          filter: "url(#goo)",
-          width: 80,
-          height: 80,
-          borderRadius: "50%",
-          background: fill ?? "#00bcd4",
-          display: "flex",
-          alignItems: "center",
-          justifyContent: "center",
-          color: "white",
-          fontWeight: "bold",
-          cursor: "pointer",
-          position: "absolute",
-          left: 0,
-          zIndex: 1,
-          top: 0,
-        }}
-        initial={{ x: -10, scale: 0.7 , y : -50}}
-        animate={{
-          y : -40,
-          x: open ? (-distance) * (i + 1) : -10,
-          scale: open ? 0.9 : 0.7,
-        }}
-        transition={{
-          type: "spring",
-          stiffness: 200,
-          damping: 30,
-          delay: i * 0.01,
-        }}
-      >
-        {nav}
-      </motion.div>
+      <Link key={i} href={`/${nav.toLowerCase()}`} passHref>
+        <motion.a
+          className={`action-button ${className}`}
+          style={{
+            filter: "url(#goo)",
+            width: 80,
+            height: 80,
+            borderRadius: "50%",
+            background: fill ?? "#00bcd4",
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+            color: "white",
+            fontWeight: "bold",
+            cursor: "pointer",
+            position: "absolute",
+            left: 0,
+            zIndex: 1,
+            top: 0,
+          }}
+          initial={{ x: -10, scale: 0.7 , y : -50}}
+          animate={{
+            y : -40,
+            x: open ? (-distance) * (i + 1) : -10,
+            scale: open ? 0.9 : 0.7,
+          }}
+          transition={{
+            type: "spring",
+            stiffness: 200,
+            damping: 30,
+            delay: i * 0.05,
+          }}
+        >
+          {nav}
+        </motion.a>
+      </Link>
     ))
 
     return (
@@ -117,8 +118,24 @@ const ActionButtons = forwardRef<ActionButtonRef, ActionButtonProps>(
           animate={{
             scale: isShrunk ? 1 : 1.5, // shrink or return
           }}
-        >
-          Store
+        > 
+          <motion.div className="hamburger flex flex-col gap-3">
+            <motion.span
+              className="hamburger-1 w-[30px] h-[3px] bg-amber-50"
+              animate={open ? { rotate: 45, y: 18, width : 36 } : { rotate: 0, y: 0 }}
+              transition={{ duration: 0.3 }}
+            />
+            <motion.span
+              className="hamburger-2 w-[30px] h-[3px] bg-amber-50"
+              animate={open ? { scaleX: 0 } : { scaleX: 1 }}
+              transition={{ duration: 0.3 }}
+            />
+            <motion.span
+              className="hamburger-3 w-[30px] h-[3px] bg-amber-50"
+              animate={open ? { rotate: -45, y: -12, width : 36 } : { rotate: 0, y: 0 }}
+              transition={{ duration: 0.3 }}
+            />
+          </motion.div>
           
         </motion.div>
         <div className="group-div-goo" style={{ filter: "url(#goo)", position : "absolute" , zIndex : 1, paddingRight : 80 }}>
--- Frontend/src/components/Navigation/NavBar.tsx
+++ Frontend/src/components/Navigation/NavBar.tsx
@@ -2,14 +2,30 @@
 import React from 'react'
 import '../../app/globals.css'
 import ActionButtonGroup from './Buttons/ActionButtonGroup'
+import Image from 'next/image'
+import brandImage from '../../../public/icons/brandIcon.png'
+import localFont from 'next/font/local';
+
+const InterFont = localFont({
+  src: '../../../public/fonts/Inter-VariableFont_opsz,wght.ttf',
+  variable: '--font-inter', // optional, for CSS variable
+})
+
 
 const NavBar : React.FC = () => {
   
   return (
-    <div className='nav-bar-container bg-amber-50'>
-      <nav className='nav-bar w-full'>
-        <section className='navigation-section flex p-10 px-10 gap-[800px]'>
-          <div className='w-2xs'>hello</div>
+    <div className='nav-bar-container bg-[#944f23] flex justify-center p-4'>
+      <nav className='nav-bar bg-[#935935] max-w-[1500px] w-full rounded-[30px] shadow-2xl'>
+        <section className='navigation-section flex justify-between px-25 py-2'>
+          <div className={`logo flex items-center gap-5 font-extrabold text-[23px]`}>
+            <div className='bg-[#754b31] rounded-[100px] '>
+              <Image src={brandImage} alt="BatCaveLogo" width={190} height={190}/>
+            </div>
+            <div className={`text-[35px] ${InterFont.className}`}>
+              Cave Café.
+            </div>
+          </div>
           <ActionButtonGroup />
         </section>  
       </nav>
